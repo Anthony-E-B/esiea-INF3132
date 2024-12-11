c1d2d70526ed62da1f4cd959674c472b4ada1762
Perrin le Louarn <lelouarnperrin@gmail.com>
2024-12-11 09:30:19 +0100

    feat: addition of a base parser

---
+++ src/main/java/INF3132/parser/BaseParser.java
@@ -0,0 +1,58 @@
+package INF3132.parser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class BaseParser<T> {
+    private final BufferedReader reader;
+
+    public BaseParser(String path) throws IOException {
+        this.reader = new BufferedReader(new FileReader(path));
+    }
+
+    protected abstract T parseBlock(Map<String, String> blockData);
+
+    /**
+     * A function that reads one block and returns the data within it.
+     * @param patternStart The pattern that defines where the block starts.
+     * @param patternEnd the pattern that defines where the block ends.
+     */
+    protected Map<String, String> readBlock(String patternStart, String patternEnd) throws IOException {
+        Map<String, String> blockData = new HashMap<>();
+        String l;
+        while((l = reader.readLine()) != null){
+            l = l.trim();
+            if(!l.equals(patternStart)) break;
+            while((l = reader.readLine()) != null){
+                l = l.trim();
+                if(l.equals(patternEnd)) break;
+                String[] value = l.split(" ", 2);
+                if(value.length == 2) blockData.put(value[0], value[1]);
+            }
+        }
+        return blockData;
+    }
+
+    /**
+     * A function that fully reads a file according to a start pattern and an end pattern. It then returns all the blocks of data that have been found.
+     * @param patternStart The pattern that defines where the block starts.
+     * @param patternEnd The pattern that defines where the block ends.
+     */
+    public List<T> parseFull(String patternStart, String patternEnd) throws IOException {
+        List<T> result = new ArrayList<>();
+        Map<String, String> blockData; 
+        while(!(blockData = readBlock(patternStart, patternEnd)).isEmpty()){
+            result.add(parseBlock(blockData));
+        }
+        return result;
+    }
+
+    public void close() throws IOException {
+        reader.close();
+    }
+}
