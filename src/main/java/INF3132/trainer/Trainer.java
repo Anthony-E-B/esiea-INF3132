50319a0cc3449e94e0a73ed66b5bae1014717a1b
Anthony Bertrand <anthony.e.bertrand@gmail.com>
2025-01-03 13:19:51 +0100

    feat(attack): Add the `orderMonsterToAttack` method on Trainer

--- src/main/java/INF3132/trainer/Trainer.java
+++ src/main/java/INF3132/trainer/Trainer.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import INF3132.monsters.Monster;
 import INF3132.monsters.exception.UnownedMonsterException;
 import INF3132.monsters.exception.MonsterUnableToFightException;
+import INF3132.attacks.Attack;
+import INF3132.attacks.exception.AttackFailedException;
 import INF3132.items.exception.UnownedItemException;
 import INF3132.items.exception.UnusableItemException;
 import INF3132.items.subclasses.Consumable;
@@ -25,6 +27,25 @@ public class Trainer {
         if (!bag.itemIsOwn(i)) throw new UnownedItemException();
 
         i.use(m);
+    }
+
+    /**
+     * Order a Monster to attack with its base attack.
+     * @param source The {@link Monster} to command.
+     * @param target The {@link Monster} to attack.
+     */
+    public void orderMonsterToAttack(Monster source, Monster target) throws AttackFailedException {
+        source.attack(target);
+    }
+
+    /**
+     * Order a Monster to attack with {@link Attack} {@param a}
+     * @param source The {@link Monster} to command.
+     * @param target The {@link Monster} to attack.
+     * @param a The {@link Attack} to use.
+     */
+    public void orderMonsterToAttack(Monster source, Monster target, Attack a) throws AttackFailedException {
+        source.attack(target, a);
     }
 
     /**
