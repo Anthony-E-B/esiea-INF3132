5a17457233241756849581ef6d6574374b83c1ca
Perrin Le Louarn <lelouarnperrin@gmail.com>
2025-01-30 19:39:31 +0100

    feat: Add Nature base behaviour

--- src/main/java/INF3132/monsters/subclasses/GrassMonster.java
+++ src/main/java/INF3132/monsters/subclasses/GrassMonster.java
@@ -3,6 +3,9 @@ package INF3132.monsters.subclasses;
 import java.util.List;
 
 import INF3132.attacks.Attack;
+import INF3132.attacks.exception.AttackFailedException;
+import INF3132.combat.Combat;
+import INF3132.combat.terrain.Terrain;
 import INF3132.monsters.FloodAffectedMonster;
 import INF3132.monsters.Monster;
 import INF3132.monsters.MonsterType;
@@ -18,6 +21,25 @@ public class GrassMonster extends Monster implements FloodAffectedMonster {
         List<Attack> attacks
     ) {
         super(name, MonsterType.GRASS, hpMax, attack, defense, speed, attacks);
+    }
+
+    @Override
+    public void beforeAttack() throws AttackFailedException {
+        super.beforeAttack();
+        Combat c = Combat.getCurrentCombat();
+        Terrain t = c.getTerrain();
+        if(t.isFlooded()){
+            c.sendMessage(String.format(
+                "%s se regénère grâce au terrain inondé !",
+                this.getName()
+            ));
+            this.restoreHealth(this.getMaxHp()/20);
+        }
+    }
+
+    @Override
+    public void afterAttack(float inflictedDamage){
+        this.afterAttack(inflictedDamage, null);
     }
 
     @Override
